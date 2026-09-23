71bd5772b69460056d88a36d9a5291e098a9a356
Archana Maurya <126747758+Archana083@users.noreply.github.com>
2026-09-23 16:33:37 +0530

    feat: add trainers route

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Adminlayout from "@/layouts/AdminLayout.vue";
 import Dashboard from "@/pages/admin/Dashboard.vue";
 import Clients from "@/pages/admin/Clients.vue";
+import Trainers from "@/pages/admin/Trainers.vue";
 
 
 const router = createRouter({
@@ -25,6 +26,11 @@ const router = createRouter({
                     path: "clients",
                     name: "admin-clients",
                     component: Clients,
+                },
+                {
+                    path: "trainers",
+                    name: "admin-trainers",
+                    component: Trainers,
                 }
             ]
         }
