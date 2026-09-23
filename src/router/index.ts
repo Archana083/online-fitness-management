f394bf74fd4f2922f6ca365d9b67bd1c33e90ee7
Archana Maurya <126747758+Archana083@users.noreply.github.com>
2026-09-23 19:19:03 +0530

    feat: add workout plans route

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import Clients from "@/pages/admin/Clients.vue";
 import Trainers from "@/pages/admin/Trainers.vue";
 import Programs from "@/pages/admin/Programs.vue";
 import Exercises from "@/pages/admin/Exercises.vue";
+import WorkoutPlans from "@/pages/admin/WorkoutPlans.vue";
 
 
 const router = createRouter({
@@ -43,6 +44,11 @@ const router = createRouter({
                     path: "exercises",
                     name: "admin-exercises",
                     component: Exercises,
+                },
+                {
+                    path: "workout-plans",
+                    name: "admin-workout-plans",
+                    component: WorkoutPlans,
                 }
             ]
         }
